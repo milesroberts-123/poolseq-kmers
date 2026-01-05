6393bf0ac3a9291e8c7b80d86c2e0b73f82117e7
Miles Roberts <milesdroberts@gmail.com>
2026-01-05 14:05:26 -0800

    update planpan prep

--- resources/02_prep_panplant_data.bash
+++ resources/02_prep_panplant_data.bash
@@ -5,7 +5,9 @@ mamba activate bcftools
 # path to reference genome
 # chromosome names should be same as vcf
 # all letters should be capital
-ref=/global/scratch/users/milesroberts/tmp/arabidopsis_thaliana_tair10_ncbi/data/GCF_000001735.4/at_upper.fa
+#ref=/global/scratch/users/milesroberts/tmp/arabidopsis_thaliana_tair10_ncbi/data/GCF_000001735.4/at_upper.fa
+#ref=/global/scratch/users/milesroberts/tmp/brassica_rapa_plantpan_data/br.genome.gene/Chiifu.genome.fasta
+ref=/global/scratch/users/milesroberts/tmp/brassica_napus_plantpan_data/bn.genome.gene/zs11_upper.fa
 
 # loop through variant types from plantpan
 #declare -a vartype=("snp" "ins" "cpg" "cpl" "del" "dup" "hdr" "inval" "invdp" "invtr" "tdm" "trans")
@@ -61,46 +63,14 @@ done
 echo Concatenating all variants...
 bcftools concat -a -Oz -o all.vcf.gz all.*.vcf.gz
 
-# realign indels and remove duplicatesi
+# realign indels and remove duplicates
+# merge bi-alleleic records
 echo Normalizing variants...
-bcftools norm -m +any -d all -o norm.vcf.gz -O z -c s -f $ref all.vcf.gz
+bcftools norm -m +any -d all -o norm.vcf.gz -Oz -f $ref all.vcf.gz
 
 # filter out rare variants
 echo Filtering out rare variants...
 bcftools view -v snps,indels -o mac2.vcf -i 'MAC > 0' norm.vcf.gz
-
-# remove any variants with complex mutations that overlap with the simpler mutations
-#echo Removing overlapping variants...
-#zcat mac2.vcf.gz | awk '$3 !~ /CPG|DUP|INV|TDM|CPL|TRANS/ || $1 ~ /^#/' > nonoverlap.vcf
-
-# create genome file for bedtools
-#echo Creating genome file...
-#samtools faidx $ref
-#awk '{print $1"\t"$2}' $ref.fai > $ref.genome
-
-# create variant flanks
-#echo Creating variant flanks...
-#bcftools query -f '%CHROM\t%POS0\t%END\n' mac2.vcf.gz > mac2.bed
-#bedtools flank -i mac2.bed -g at.genome -b 36 > flanks.bed
-
-# subtract flanks from variants to get variants not near other variants
-#echo Getting isolated variants...
-#bedtools subtract -A -a mac2.vcf.gz -b flanks.bed > isolated.vcf
-#bcftools view -h mac2.vcf.gz > header.vcf
-#cat header.vcf isolated.vcf > isolated_with_header.vcf
-#bgzip isolated_with_header.vcf
-#tabix isolated_with_header.vcf.gz
-
-# get only snps
-#echo Extracting only snps...
-#bcftools view -v snps isolated_with_header.vcf.gz -Oz -o snps_only.vcf.gz
-
-# some non-snps were not filtered out still
-#echo Removing DUP and TRANS...
-#zgrep -v "DUP" snps_only.vcf.gz | grep -v "TRANS" > no_dup.vcf
-
-# remove a snp near the chromosome end
-# zgrep -v "SNP687663;SNP945145;SNP725163;SNP734774;SNP795013" no_dup.vcf.gz > no_ends.vcf
 
 # capitalize letters
 echo Capitalize letters for alleles...
@@ -114,5 +84,3 @@ cat header.vcf body.vcf > final.vcf
 bgzip final.vcf
 tabix final.vcf.gz
 
-# realign indels and remove duplicates
-#bcftools norm -d all -o final.vcf.gz -O z -c s -f at.fa capital.vcf.gz
